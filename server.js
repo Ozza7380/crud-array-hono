6356941dc4c1e8cde805ca2b631aa6e4316250b0
M Raihan H <ar937464@gmail.com>
2026-06-25 11:01:56 +0700

    read

---
+++ server.js
@@ -0,0 +1,20 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
+// data array - penyimpanan sementara di memory
+let items = [
+    { id: 1, nama: "belajar Hono", status: "selesai" },
+    { id: 2, nama: "belajar API", status: "belum"},
+]
+
+//counter untuk id otomatis
+let nextId = 3
+
+const app = new Hono()
+
+app.get(`/api/items`, (c) => {
+    return c.json({eror: false, data: items})
+})
+
+serve({ fetch: app.fetch, port:3000  })
+console.log(`API berjalan di http://localhost:3000`)
