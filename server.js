acb62b37d30246106d9b13ead952c83f5ebb0203
M Raihan H <ar937464@gmail.com>
2026-06-25 11:13:19 +0700

    read one

--- server.js
+++ server.js
@@ -13,8 +13,22 @@ let nextId = 3
 const app = new Hono()
 
 //membaca data (read)
+
+//read all
 app.get(`/api/items`, (c) => {
     return c.json({eror: false, data: items})
+})
+
+//read one
+app.get(`/api/items/:id`, (c) => {
+    const id = Number(c.req.param(`id`))
+    const item = items.find((i) => i.id === id)
+
+    if (!item) {
+        return c.json({ error: true, massage: `item tidak ditemukan` }, 404)
+    }
+
+    return c.json({ error:false, data: item })
 })
 
 serve({ fetch: app.fetch, port:3000  })
