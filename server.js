07a0d79675808c596500243e54dcf3339324d18c
M Raihan H <ar937464@gmail.com>
2026-06-25 12:05:25 +0700

    mengubah data(put)

--- server.js
+++ server.js
@@ -51,5 +51,23 @@ app.post(`/api/items`, async (c) => {
     return c.json({ error: false, data: itemBaru }, 201)
 })
 
+//mengubah data (Update)-put
+app.put(`/api/items/:id`, async (c) => {
+    const id = Number(c.req.param(`id`))
+    const body = await c.req.json()
+
+    const index = items.findIndex((i) => i.id === id)
+
+    if (index === -1) {
+        return c.json({ error: true, massage: `ite tidak ditemukan` }, 404)
+    }
+
+    //update hanya field yang dikirim
+    if (body.nama !== undefined) items[index].nama = body.nama
+    if (body.status !== undefined) items[index].status = body.status
+
+    return c.json({ error: false, data: items[index] })
+})
+
 serve({ fetch: app.fetch, port:3000  })
 console.log(`API berjalan di http://localhost:3000`)
